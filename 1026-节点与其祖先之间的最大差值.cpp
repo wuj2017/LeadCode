3de7b3a68c007974f612d65dfae8187abf2e214c
wujin <goodCoder@wj.com>
2019-04-24 19:31:58 +0800

    节点与其祖先之间的最大差值

---
+++ 1026-节点与其祖先之间的最大差值.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root) {
+		int res = 0;
+		Diff(root, res);
+		return res;
+	}
+	void Diff(TreeNode* pNode,int &res)
+	{
+		if (!pNode) return;
+		if (pNode)
+		{
+			int root_val = pNode->val;
+			calculateDiff(pNode, root_val, res);
+			Diff(pNode->left,res);
+			Diff(pNode->right, res);
+		}
+	}
+	void calculateDiff(TreeNode* pNode,int root_val,int &res)
+	{
+        if (!pNode) return;
+		if (pNode)
+		{
+			res = max(res, abs(root_val - pNode->val));
+			calculateDiff(pNode->left, root_val, res);
+			calculateDiff(pNode->right, root_val, res);
+		}
+	}
+};

89554049d3156dbb8b32efcc55cfbcb14ef496f6
wujin <goodCoder@wj.com>
2019-04-30 20:49:11 +0800

    两地调度

---
+++ 1029-两地调度.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static bool cmp( vector<int> a, vector<int> b){
+        return a[0] - a[1] > b[0] - b[1];
+    }
+
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int res = 0;
+        
+        sort( costs.begin(), costs.end(), cmp);
+  
+        for( int i = 0, j = costs.size()-1; i < j; i++, j--)
+            res += costs[i][1] + costs[j][0];
+        return res;
+    }
+};
